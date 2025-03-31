84730b9be93ec30ebfc8c9cdb0030512f868dc26
Alessandro N Sousa <allessandro188@gmail.com>
2025-03-31 16:11:55 -0300

    Feat: criação da funcionalidade de listar todos os eventos cadastrados com paginação.

--- src/main/java/com/eventostec/api/controller/EventController.java
+++ src/main/java/com/eventostec/api/controller/EventController.java
@@ -2,11 +2,14 @@ package com.eventostec.api.controller;
 
 import com.eventostec.api.domain.event.Event;
 import com.eventostec.api.domain.event.EventRequestDTO;
+import com.eventostec.api.domain.event.EventResponseDto;
 import com.eventostec.api.service.EventService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/event")
@@ -26,5 +29,11 @@ public class EventController {
         EventRequestDTO eventRequestDTO = new EventRequestDTO(title,description, date, city, state, remote, eventUrl, image);
         Event newEvent = this.eventService.createEvent(eventRequestDTO);
         return ResponseEntity.ok(newEvent);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<EventResponseDto>> getEvents(@RequestParam (defaultValue = "") int page, @RequestParam(defaultValue = "10") int size) {
+        List<EventResponseDto> allEvents = this.eventService.getEvents(page, size);
+        return ResponseEntity.ok(allEvents);
     }
 }
